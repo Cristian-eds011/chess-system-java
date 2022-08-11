6de9db1fa0cd5579b042e2736d80dcd9901a9f15
Cristian Eduardo dos Santos <cristian.santos0308@gmail.com>
2022-08-10 21:55:21 -0300

    Basic test for possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -16,4 +16,10 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return matriz;
+	}
+
 }
